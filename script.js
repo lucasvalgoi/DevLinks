5baa96ceaa741830d15a60053145b29edb88f823
lucasvalgoi <lucasluisvalgoi@gmail.com>
2026-01-12 22:24:23 -0300

    adjust code in toggle mode

--- script.js
+++ script.js
@@ -4,18 +4,13 @@ function toggleMode() {
     // função de add e remover classe light e suas propriedades
     html.classList.toggle('light');
 
+    // seleciona a imagem dentro do id profile
     const img = document.querySelector('#profile img');
     if (html.classList.contains('light')) {
         // add imagem light
         img.setAttribute('src', './assets/avatar-light.png');
-        
-        // muda texto alt da imagem
-        img.setAttribute('alt', 'Foto de Lucas Valgoi sorrindo com óculos de sol e camisa cinza na praia');
     } else {
         // add imagem dark
         img.setAttribute('src', './assets/avatar.png');
-
-        // muda texto alt da imagem
-        img.setAttribute('alt', 'Foto de Lucas Valgoi sorrindo com camisa preta e fundo branco escuro');
     }
 }
